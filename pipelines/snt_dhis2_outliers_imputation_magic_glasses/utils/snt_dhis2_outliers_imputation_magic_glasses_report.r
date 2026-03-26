1dbb87fa98d15dbcf31406f826dad87db5481d53
claude-marie <croussaux@bluesquarehub.com>
2026-03-26 15:18:36 +0100

    fix

--- pipelines/snt_dhis2_outliers_imputation_magic_glasses/utils/snt_dhis2_outliers_imputation_magic_glasses_report.r
+++ pipelines/snt_dhis2_outliers_imputation_magic_glasses/utils/snt_dhis2_outliers_imputation_magic_glasses_report.r
@@ -1,5 +1,11 @@
 # Report helpers for magic glasses outliers imputation pipeline.
 .this_file <- tryCatch(normalizePath(sys.frame(1)$ofile), error = function(e) NA_character_)
-.this_dir <- if (!is.na(.this_file)) dirname(.this_file) else getwd()
+.this_dir <- if (exists("PIPELINE_PATH", inherits = TRUE)) {
+    file.path(get("PIPELINE_PATH", inherits = TRUE), "utils")
+} else if (!is.na(.this_file)) {
+    dirname(.this_file)
+} else {
+    getwd()
+}
 source(file.path(.this_dir, "snt_dhis2_outliers_imputation_magic_glasses.r"))
 
--- pipelines/snt_dhis2_outliers_imputation_median/utils/snt_dhis2_outliers_imputation_median_report.r
+++ pipelines/snt_dhis2_outliers_imputation_median/utils/snt_dhis2_outliers_imputation_median_report.r
@@ -1,6 +1,12 @@
 # Report helpers for median outliers imputation pipeline.
 .this_file <- tryCatch(normalizePath(sys.frame(1)$ofile), error = function(e) NA_character_)
-.this_dir <- if (!is.na(.this_file)) dirname(.this_file) else getwd()
+.this_dir <- if (exists("PIPELINE_PATH", inherits = TRUE)) {
+    file.path(get("PIPELINE_PATH", inherits = TRUE), "utils")
+} else if (!is.na(.this_file)) {
+    dirname(.this_file)
+} else {
+    getwd()
+}
 source(file.path(.this_dir, "snt_dhis2_outliers_imputation_median.r"))
 
 printdim <- function(df, name = deparse(substitute(df))) {

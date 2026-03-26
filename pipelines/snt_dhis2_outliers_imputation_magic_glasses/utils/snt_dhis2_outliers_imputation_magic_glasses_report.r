b5f9e3d02360a7a1202d3661300ec09e1efaf921
claude-marie <croussaux@bluesquarehub.com>
2026-03-26 17:24:12 +0100

    still bugged

--- pipelines/snt_dhis2_outliers_imputation_magic_glasses/utils/snt_dhis2_outliers_imputation_magic_glasses_report.r
+++ pipelines/snt_dhis2_outliers_imputation_magic_glasses/utils/snt_dhis2_outliers_imputation_magic_glasses_report.r
@@ -1,11 +1,24 @@
 # Report helpers for magic glasses outliers imputation pipeline.
 .this_file <- tryCatch(normalizePath(sys.frame(1)$ofile), error = function(e) NA_character_)
-.this_dir <- if (exists("PIPELINE_PATH", inherits = TRUE)) {
-    file.path(get("PIPELINE_PATH", inherits = TRUE), "utils")
-} else if (!is.na(.this_file)) {
-    dirname(.this_file)
-} else {
-    getwd()
+.candidate_files <- unique(c(
+    if (exists("PIPELINE_PATH", inherits = TRUE)) {
+        file.path(get("PIPELINE_PATH", inherits = TRUE), "utils", "snt_dhis2_outliers_imputation_magic_glasses.r")
+    } else {
+        character(0)
+    },
+    if (!is.na(.this_file)) {
+        file.path(dirname(.this_file), "snt_dhis2_outliers_imputation_magic_glasses.r")
+    } else {
+        character(0)
+    },
+    file.path(getwd(), "snt_dhis2_outliers_imputation_magic_glasses.r")
+))
+.target_file <- .candidate_files[file.exists(.candidate_files)][1]
+if (is.na(.target_file)) {
+    stop(paste0(
+        "Could not locate snt_dhis2_outliers_imputation_magic_glasses.r. Tried: ",
+        paste(.candidate_files, collapse = " | ")
+    ))
 }
-source(file.path(.this_dir, "snt_dhis2_outliers_imputation_magic_glasses.r"))
+source(.target_file)
 

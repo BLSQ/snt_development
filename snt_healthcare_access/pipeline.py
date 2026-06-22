2383b0bb2c861a7f768ccf3f4da0fdfd211f7a0c
irautu-bluesquare <irautu@bluesquarehub.com>
2026-06-22 21:23:20 +0200

    healthcare access - fixed whitespaces (#99)
    * (fix) healthcare access: fix whitespaces in pipeline
    
    * (docs) healthcare access: doc

--- snt_healthcare_access/pipeline.py
+++ snt_healthcare_access/pipeline.py
@@ -73,14 +73,14 @@ def snt_healthcare_access(
     data_output_path.mkdir(parents=True, exist_ok=True)
     data_intermediate_path.mkdir(parents=True, exist_ok=True)
 
-    # validate input parameter values
-    if input_fosa_file is None:  
-        current_run.log_info("Using default FOSA data (DHIS2).")  
-    elif not Path(input_fosa_file.path).exists():  
-        current_run.log_warning("Input FOSA file not found. Check the path and rerun pipeline.")  
-    elif not has_allowed_extension(input_fosa_file):  
+    # validate input parameter values (all in one)
+    if input_fosa_file is None:
+        current_run.log_info("Using default FOSA data (DHIS2).")
+    elif not Path(input_fosa_file.path).exists():
+        current_run.log_warning("Input FOSA file not found. Check the path and rerun pipeline.")
+    elif not has_allowed_extension(input_fosa_file):
         current_run.log_warning("FOSA location file should be a .csv file. Using default FOSA data instead.")
-    else:  
+    else:
         current_run.log_info(f"Using FOSA coordinates file: {input_fosa_file.path}")
 
     if not (2015 <= wpop_year <= 2030):

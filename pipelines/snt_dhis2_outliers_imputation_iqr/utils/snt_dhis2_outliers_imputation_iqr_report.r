42e02473a3c74443307559f3d6cbd1b60e3483a5
iulia rautu <irautu@bluesquarehub.com>
2026-09-15 19:10:42 +0200

    perf(outlier detection): harmonize plot inlines

--- pipelines/snt_dhis2_outliers_imputation_iqr/utils/snt_dhis2_outliers_imputation_iqr_report.r
+++ pipelines/snt_dhis2_outliers_imputation_iqr/utils/snt_dhis2_outliers_imputation_iqr_report.r
@@ -186,7 +186,7 @@ get_coherence_definitions <- function() {
   list(checks = checks, check_labels = check_labels)
 }
 
-compute_national_coherency_metrics <- function(df, checks, check_labels) {
+compute_national_coherence_metrics <- function(df, checks, check_labels) {
   df_checks <- df %>%
     dplyr::mutate(
       !!!lapply(names(checks), function(check_name) {
@@ -228,8 +228,8 @@ compute_national_coherency_metrics <- function(df, checks, check_labels) {
     )
 }
 
-plot_national_coherence_heatmap <- function(coherency_metrics) {
-  ggplot2::ggplot(coherency_metrics, ggplot2::aes(
+plot_national_coherence_heatmap <- function(coherence_metrics) {
+  ggplot2::ggplot(coherence_metrics, ggplot2::aes(
     x = factor(.data$YEAR),
     y = .data$check_label,
     fill = .data$pct_coherent
